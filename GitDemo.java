11df31ee6d0a31bc9e2f8811b98ccd0b2f625772
dpike99 <79556032+dpike99@users.noreply.github.com>
2021-02-27 20:29:15 -0700

    Adding code to addNumber and reverseNumber
    previousNumber = previousNumber + num ;

--- GitDemo.java
+++ GitDemo.java
@@ -1,10 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Devin Pike 
  ******/
 public class GitDemo {
     
     private double previousNumber;
-    
+    private double reverseNum;
     public GitDemo() {
         previousNumber = 1;
     }
@@ -14,6 +14,7 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
+        previousNumber = num + previousNumber;
         return 0;
     }
     
@@ -45,7 +46,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum = newNumber;
         return newNumber;
     }
     
